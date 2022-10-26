d73f6d83d7577ce94b89a048a0434f065c4b8ed3
EGonzalez <egonzalez12@miumg.edu.gt>
2022-10-25 23:49:55 -0600

    Program

---
+++ ejercicio2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    class Altura_Personas
+    {
+        private float[] alturas;
+        private float promedio;
+
+        public void Cargar() 
+        {
+            alturas=new float[5];
+            for (int f = 0; f < 5; f++)
+            {
+                Console.Write("Ingrese la altura de la persona:  ");
+                string linea = Console.ReadLine();
+                alturas[f] = float.Parse(linea);
+            }
+        }
+    
+        public void CalcularPromedio() 
+        {
+            float suma;
+            suma=0;
+            for(int f=0; f < 5; f++) 
+            {
+                suma=suma+alturas[f];
+            }
+            promedio=suma/5;
+            Console.WriteLine("Promedio de altura: "+promedio);
+        }
+
+        public void MayoresMenores() 
+        {
+            int may,men;
+            may=0;
+            men=0;
+            for(int f = 0; f < 5; f++) 
+            {
+                if (alturas[f] > promedio) 
+                {
+	                may++;
+                }
+                else
+                {
+                    if (alturas[f] < promedio) 
+                    {
+                        men++;
+                    }
+                }
+            }
+            Console.WriteLine("Personas mayores al promedio:  "+may);
+            Console.WriteLine("Personas menores al promedio:  "+men);
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Altura_Personas pv2 = new Altura_Personas();
+            pv2.Cargar();
+            pv2.CalcularPromedio();
+            pv2.MayoresMenores();
+        }
+    }
+}                       
---
+++ ejercicio5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio5
+{
+    class Matriz_4x4
+    {
+        private int[,] mat;
+
+        public void cargar() 
+        {
+            mat=new int[4,4];
+            for(int f = 0; f < 4; f++) 
+            {
+                for(int c = 0; c<4; c++) 
+                {
+                    Console.Write("Ingrese posicion  ["+(f+1)+","+(c+1)+"]: ");
+                    string linea;
+                    linea = Console.ReadLine();
+                    mat[f, c] = int.Parse(linea);
+                }
+            }
+        }
+       
+            
+        public void ImprimirDiagonalPrincipal() 
+        {
+            for(int k = 0; k < 4; k++) 
+            {
+                Console.Write(mat[k,k]+" ");
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Matriz_4x4 ma = new Matriz_4x4();
+            ma.cargar();
+            ma.ImprimirDiagonalPrincipal();
+        }
+    }
+}
